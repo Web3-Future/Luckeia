b14fdc78b8c7e4a69f7d7cf4f31e5344243d2904
Alanyin <alanyin6@gmail.com>
2024-05-23 14:40:11 +0800

    feat: init lib and contract

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lucky-chain
+
+
+## quick start 
+
+```
+forge install smartcontractkit/chainlink --no-commit
+forge install smartcontractkit/chainlink --no-commit
+```
---
+++ contract/src/LuckyDraw.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+// import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+// import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+contract LuckyDraw {
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    // prepare
+    // claim
+}
